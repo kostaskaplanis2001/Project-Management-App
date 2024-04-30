4ea3402d2e0efd0191b6e8ad4c504ce2cd988cbd
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-30 16:52:44 +0300

    Styling the Modal via Tailwind CSS and added Cancel functionality

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import Modal from "./Modal";
 import Input from "./Input";
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
     const modal = useRef();
 
     const title = useRef();
@@ -33,13 +33,15 @@ export default function NewProject({ onAdd }) {
     return (
         <>
             <Modal ref={modal} buttonCaption="Okay">
-                <h2>Invalid Input</h2>
-                <p>Oops ... looks like you forgot to enter a value.</p>
-                <p>Please make sure you provide a valid value for every input field. </p>
+                <h2 className='text-xl font-bold text-stone-500 my-4'>Invalid Input</h2>
+                <p className="text-stone-800 mb-4">Oops ... looks like you forgot to enter a value.</p>
+                <p className="text-stone-800 mb-4">Please make sure you provide a valid value for every input field. </p>
             </Modal >
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
-                    <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                    <li><button className="text-stone-800 hover:text-stone-950"
+                        onClick={onCancel}
+                    >Cancel</button></li>
                     <li><button
                         className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
                         onClick={handleSave}
